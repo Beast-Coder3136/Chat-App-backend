93824fd50898647f333f6bd2fc21365d9274eae0
Anas <mohdanasali3136@gamil.com>
2026-03-01 23:13:53 +0530

    change cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://chat-app-backend-4hs0.onrender.com",
   credentials: true
 }))
 app.use(cookieParser());
